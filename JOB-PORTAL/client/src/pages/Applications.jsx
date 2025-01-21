6b31ac58e5dd839913cf6e07a1483c2003b5a865
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2025-01-21 20:07:30 +0530

    half done application page

--- JOB-PORTAL/client/src/pages/Applications.jsx
+++ JOB-PORTAL/client/src/pages/Applications.jsx
@@ -1,10 +1,85 @@
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { assets, jobsApplied } from "../assets/assets";
+import moment from 'moment'
 
 const Applications = () => {
+  const [isEdit, setisEdit] = useState(false);
+  const [resume, setResume] = useState(null);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10">
+        <h2 className="text-xl font-semibold">Your Resume</h2>
+        <div className="flex gap-2 mb-6 mt-3">
+          {isEdit ? (
+            <>
+              <label className="flex items-center" htmlFor="resumeUpload">
+                <p className="bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2">
+                  Select Resume
+                </p>
+                <input
+                  id="resumeUpload"
+                  onChange={(e) => setResume(e.target.files[0])}
+                  accept="application/pdf"
+                  type="file"
+                  hidden
+                />
+                <img src={assets.profile_upload_icon} />
+              </label>
+              <button
+                onClick={(e) => setisEdit(false)}
+                className="bg-green-100 border border-green-400 rounded-lg px-4 py-2"
+              >
+                Save
+              </button>
+            </>
+          ) : (
+            <div className="flex gap-2">
+              <a
+                className="bg-purple-100 text-purple-700 px-4 py-2 rounded hover:bg-purple-300"
+                href=""
+              >
+                Resume
+              </a>
+              <button
+                onClick={() => setisEdit(true)}
+                className="text-gray-500 border border-gray-300 rounded-lg px-4 py-2 hover:bg-gray-200"
+              >
+                Edit
+              </button>
+            </div>
+          )}
+        </div>
+        <h2 className="text-xl font-semibold mb-4">Jobs Applied</h2>
+        <table className="min-w-full bg-white border rounded-lg">
+          <thead>
+            <tr>
+              <th className="py-3 px-4 border-b text-left">Company</th>
+              <th className="py-3 px-4 border-b text-left">Job Title</th>
+              <th className="py-3 px-4 border-b text-left max-sm:hidden">Location</th>
+              <th className="py-3 px-4 border-b text-left max-sm:hidden">Date</th>
+              <th className="py-3 px-4 border-b text-left">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobsApplied.map((job, index) => true ?(
+              // job.jobId
+                    <tr key={index}>
+                      <td className="py-3 px-4 flex items-center gap-2 border-b"><img src={job.logo}/>{job.company}</td>
+                      <td className="py-2 px-4 border-b">{job.title}</td>
+                      <td className="py-2 px-4 border-b max-sm:hidden">{job.location}</td>
+                      <td className="py-2 px-4 border-b max-sm:hidden">{moment(job.date).format('ll')}</td>
+                      <td className="py-2 px-4 border-b">{job.status}</td>
+                    </tr>
+            ):(null) )}
+          </tbody>
+        </table>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Applications
+export default Applications;
