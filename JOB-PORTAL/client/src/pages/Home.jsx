b7e3d0821bc7749a4297d1916b7227127713fc29
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2025-01-21 15:55:57 +0530

    Footer section added

--- JOB-PORTAL/client/src/pages/Home.jsx
+++ JOB-PORTAL/client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer"
 import Hero from "../components/Hero"
 import JobListing from "../components/JobListing"
 import Navbar from "../components/Navbar"
@@ -8,6 +9,7 @@ const Home = () => {
       <Navbar/>
       <Hero/>
       <JobListing/>
+      <Footer/>
     </div>
   )
 }
