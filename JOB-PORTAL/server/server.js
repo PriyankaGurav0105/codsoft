9b1ea6eae514425f1701e7ffa0ada51b9f24e4d1
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2025-01-22 13:26:22 +0530

    Basic Server is ready

---
+++ JOB-PORTAL/server/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+
+//Initialize Express
+const app = express()
+
+
+//Middlewares
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.get('/',(req,res)=> res.send("API Working"))
+
+
+//Port
+const port = process.env.PORT || 5000
+
+//To run the backend
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+    })
